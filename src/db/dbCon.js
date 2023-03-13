93eee1a6152bb43e8ac35379d31f2343ca24ad35
Alexander Nygård <A.K.Nygard@student.tudelft.nl>
2023-03-13 14:33:24 +0100

    test: added in-memory mongo test database

--- src/db/dbCon.js
+++ src/db/dbCon.js
@@ -1,21 +1,29 @@
 // import { MongoClient, ServerApiVersion } from 'mongodb';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import { ScheduleModel, ActivityModel, UserModel } from './models/index';
 
 dotenv.config();
 
-const uri = process.env.MONGO_URI || '';
-const client = mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// const uri = process.env.MONGO_URI || '';
+// const client = mongoose.connect(uri, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
 
-let conn;
-try {
-    conn = await client.connect();
-} catch (e) {
-    console.error(e);
-}
+// let conn;
+// try {
+//     conn = await client.connect();
+// } catch (e) {
+//     console.error(e);
+// }
 
-const db = conn.db(process.env.MONGO_DBNAME || '');
-export default db;
+// const db = conn.db(process.env.MONGO_DBNAME || '');
+// export default db;
+
+module.exports = {
+    init: (uri) => mongoose.connect(uri || process.env.MONGO_URI),
+    close: () => mongoose.disconnect(),
+    Schedule: ScheduleModel,
+    User: UserModel,
+};
--- src/router/ScheduleController.test.js
+++ src/router/ScheduleController.test.js
@@ -1,12 +1,26 @@
 import express from 'express';
 import request from 'supertest';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 import ScheduleController from './ScheduleController.js';
+import db from '../db/dbCon.js';
 
 const app = new express();
 app.use(express.json());
 app.use('/', ScheduleController);
 
 describe('ScheduleController', () => {
+    let mongoServer;
+
+    beforeAll(async () => {
+        mongoServer = await MongoMemoryServer.create();
+        await db.init(mongoServer.getUri());
+    });
+
+    afterAll(async () => {
+        await db.close();
+        await mongoServer.stop();
+    });
+
     test('GET /', async () => {
         const response = await request(app).get('/');
         expect(response.statusCode).toBe(200);
